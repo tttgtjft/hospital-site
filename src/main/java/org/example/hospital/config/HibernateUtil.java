1b232bf742397d138e5ccfd80bfe24d37e19e59e
tttgtjft <1onecov@gmail.com>
2023-12-26 10:57:01 +0300

    added structure

---
+++ src/main/java/org/example/hospital/config/HibernateUtil.java
@@ -0,0 +1,28 @@
+package org.example.hospital.config;
+
+import lombok.Getter;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil
+{
+    @Getter
+    private static SessionFactory sessionFactory;
+
+    static
+    {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        try
+        {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e)
+        {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
